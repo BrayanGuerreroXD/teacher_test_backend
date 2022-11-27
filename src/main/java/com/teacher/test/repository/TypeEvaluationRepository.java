f7601c76200d6f28413e0a7e097bb718f2e6a19b
BrayanGuerreroXD <bg7047653@gmail.com>
2022-11-26 20:22:09 -0500

    Create TypeEvaluationRepository.java

---
+++ src/main/java/com/teacher/test/repository/TypeEvaluationRepository.java
@@ -0,0 +1,7 @@
+package com.teacher.test.repository;
+
+import com.teacher.test.entities.TypeEvaluation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TypeEvaluationRepository extends JpaRepository<TypeEvaluation, Integer> {
+}
