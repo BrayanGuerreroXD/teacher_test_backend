f206ae97a63acbd8b28dfe283e4861847877ec71
BrayanGuerreroXD <bg7047653@gmail.com>
2022-11-27 10:56:28 -0500

    Update InscriptionUserController.java

--- src/main/java/com/teacher/test/controller/InscriptionUserController.java
+++ src/main/java/com/teacher/test/controller/InscriptionUserController.java
@@ -1,5 +1,7 @@
 package com.teacher.test.controller;
 
+import com.teacher.test.entities.InscriptionUser;
+import com.teacher.test.repository.InscriptionUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -18,4 +20,58 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/inscriptions_users")
 public class InscriptionUserController {
+
+    @Autowired
+    InscriptionUserRepository inscriptionUserRepo;
+
+    @GetMapping
+    public List<InscriptionUser> getInscriptionsUsersAll() {
+        return inscriptionUserRepo.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public InscriptionUser getInscriptionsUsersbyId(@PathVariable Integer id) {
+        Optional<InscriptionUser> inscriptionUser = inscriptionUserRepo.findById(id);
+
+        if (inscriptionUser.isPresent()) {
+            return inscriptionUser.get();
+        }
+
+        return null;
+    }
+
+    @PostMapping
+    public InscriptionUser postInscriptionsUsers(@RequestBody InscriptionUser inscriptionUser) {
+        inscriptionUserRepo.save(inscriptionUser);
+        return inscriptionUser;
+    }
+
+
+    @PutMapping("/{id}")
+    public InscriptionUser putInscriptionsUsersbyId(@PathVariable Integer id, @RequestBody InscriptionUser inscriptionUser) {
+        Optional<InscriptionUser> inscriptionUserCurrent = inscriptionUserRepo.findById(id);
+
+        if (inscriptionUserCurrent.isPresent()) {
+            InscriptionUser inscriptionUserReturn = inscriptionUserCurrent.get();
+            inscriptionUserReturn.setInscription(inscriptionUser.getInscription());
+            inscriptionUserReturn.setUser(inscriptionUser.getUser());
+            inscriptionUserRepo.save(inscriptionUserReturn);
+            return inscriptionUserReturn;
+        }
+
+        return null;
+    }
+
+    @DeleteMapping("/{id}")
+    public InscriptionUser deleteInscriptionsUsersbyId(@PathVariable Integer id) {
+        Optional<InscriptionUser> inscriptionUser = inscriptionUserRepo.findById(id);
+
+        if (inscriptionUser.isPresent()) {
+            InscriptionUser inscriptionUserReturn = inscriptionUser.get();
+            inscriptionUserRepo.deleteById(id);
+            return inscriptionUserReturn;
+        }
+
+        return null;
+    }
 }
