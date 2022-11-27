ea647dc8fd99f44f4fa59dd3062e0d21db103be9
BrayanGuerreroXD <bg7047653@gmail.com>
2022-11-27 11:51:32 -0500

    Update Criterion.java

--- src/main/java/com/teacher/test/entities/Criterion.java
+++ src/main/java/com/teacher/test/entities/Criterion.java
@@ -7,6 +7,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
+import javax.persistence.Column;
 import javax.persistence.Table;
 
 @Entity
@@ -19,9 +20,12 @@ public class Criterion {
     @GeneratedValue(strategy= GenerationType.SEQUENCE, generator="criterion_id_seq")
     private Integer id;
 
+    @Column(name = "max_value")
     private int maxValue;
 
+    @Column(name = "min_value")
     private int minValue;
 
+    @Column(name = "value")
     private int value;
 }
