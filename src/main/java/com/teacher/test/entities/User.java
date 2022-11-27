a595a5868bc516ab82fb2f484f0ba8321611cf7a
BrayanGuerreroXD <bg7047653@gmail.com>
2022-11-27 11:46:52 -0500

    Update User.java

--- src/main/java/com/teacher/test/entities/User.java
+++ src/main/java/com/teacher/test/entities/User.java
@@ -5,6 +5,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
+import javax.persistence.Column;
 import javax.persistence.Table;
 
 import lombok.Data;
@@ -19,15 +20,21 @@ public class User {
     @GeneratedValue(strategy= GenerationType.SEQUENCE, generator="user_id_seq")
     private Integer id;
 
+    @Column(name="user_name")
     private String userName;
 
+    @Column(name="user_code")
     private int userCode;
 
+    @Column(name="identify_card_number")
     private int identifyCardNumber;
 
+    @Column(name="email")
     private String email;
 
+    @Column(name="password")
     private String password;
 
+    @Column(name="is_admin")
     private boolean isAdmin;
 }
