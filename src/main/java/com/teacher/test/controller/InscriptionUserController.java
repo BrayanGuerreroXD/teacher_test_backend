3310df5d588304d02ca5fbc9b8b8327283510e17
BrayanGuerreroXD <bg7047653@gmail.com>
2022-11-27 10:32:04 -0500

    Update InscriptionUserController by adding libraries

--- src/main/java/com/teacher/test/controller/InscriptionUserController.java
+++ src/main/java/com/teacher/test/controller/InscriptionUserController.java
@@ -1,4 +1,21 @@
 package com.teacher.test.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@RestController
+@RequestMapping("/inscriptions_users")
 public class InscriptionUserController {
 }
