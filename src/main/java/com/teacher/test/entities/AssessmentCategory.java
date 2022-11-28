cb4e9b0f6f742467aada13c58f5bd58f8b55c18f
BrayanGuerreroXD <bg7047653@gmail.com>
2022-11-27 19:23:01 -0500

    Update AssessmentCategory.java

--- src/main/java/com/teacher/test/entities/AssessmentCategory.java
+++ src/main/java/com/teacher/test/entities/AssessmentCategory.java
@@ -1,4 +1,31 @@
 package com.teacher.test.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.OneToOne;
+import javax.persistence.JoinColumn;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Table(name = "assessment_category")
+@Data
 public class AssessmentCategory {
+
+    @Id
+    @SequenceGenerator(name="assessment_category_id_seq", allocationSize=1)
+    @GeneratedValue(strategy= GenerationType.SEQUENCE, generator="assessment_category_id_seq")
+    private Integer id;
+
+    @OneToOne
+    @JoinColumn(name = "assessment_id")
+    private Assessment assessment;
+
+    @OneToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
 }
